b1eaf5309c271d0e7e7656d84769c9bc1ca2781e
DESKTOP-JHVMHUT\user1 <meguru01010101@gmail.com>
2026-07-27 08:35:54 +0900

    미니게임 메인화면과 게임파일 초기생성

---
+++ main.py
@@ -0,0 +1,34 @@
+# from game.rps import rps
+# from game.updown import updown
+# from game.lotto import lotto
+# from game.slot import slot
+
+while True:
+
+    print("\n===== 🎮 미니 게임 =====")
+    print("1. 가위바위보")
+    print("2. 업다운")
+    print("3. 로또")
+    print("4. 슬롯머신")
+    print("5. 종료")
+
+    menu = input("메뉴 선택 : ")
+
+    if menu == "1":
+        rps()
+
+    elif menu == "2":
+        updown()
+
+    elif menu == "3":
+        lotto()
+
+    elif menu == "4":
+        slot()
+
+    elif menu == "5":
+        print("👋게임을 종료합니다.")
+        break
+
+    else:
+        print("잘못된 입력입니다.")
